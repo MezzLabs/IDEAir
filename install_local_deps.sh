0ebaf61cd9209e166f5ad736cba9a260e02abf3f
Shoumeng Yan <shoumeng.yan@intel.com>
2014-10-04 14:40:00 +0800

    add pty.js patch that was missed in last commit

--- install_local_deps.sh
+++ install_local_deps.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+wget http://registry.npmjs.org/pty.js/-/pty.js-0.1.2.tgz 
+mv pty.js-0.1.2.tgz local_modules/
+cd local_modules; rm -rf package; tar xzvf pty.js-0.1.2.tgz; cd package; patch -p1 <../pty.js-0.1.2.patch
+cd ../../ 
 for i in $(find ./local_modules -maxdepth 1 -type d) ; do
     packageJson="${i}/package.json"
     if [ -f "${packageJson}" ]; then
